f500fc45e6a0ff128e3ae3ad4bae827af63c3412
isiktopcu <91604042+isiktopcu@users.noreply.github.com>
2021-12-16 14:57:31 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np;
+import unittest
+from ML_function import ML_regression
+d1 = {'Animals':[1,"dog","cat","bird"],"Fruits": ["banana",5,"orange","blueberry"]}
+df = pd.DataFrame(d1)
+X = df[['Animals']]
+Y = df[['Fruits']]
+
+class TestMLRegresion(unittest.TestCase):
+    
+    def test_input(self):#checking if the input data is a string
+        with self.assertRaises(TypeError): 
+            ML_regression(X,Y)
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+
